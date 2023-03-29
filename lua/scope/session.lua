467084c3ba06e88eb38a7ac94feb8125aeecad42
tiagovla <tiagovla@gmail.com>
2023-03-29 12:34:11 -0300

    refactor: decouple from session globals

--- lua/scope/session.lua
+++ lua/scope/session.lua
@@ -1,25 +1,22 @@
 local utils = require "scope.utils"
 local M = {}
 
-function M.load_session()
-    if vim.g.ScopeState == nil then
-        return
-    end
+function M.deserialize_state(state)
     local buf_nums = utils.get_valid_buffers()
     for _, k in pairs(buf_nums) do
         vim.api.nvim_buf_set_option(k, "buflisted", false)
     end
-    local scope_state = vim.json.decode(vim.g.ScopeState)
+    local scope_state = vim.json.decode(state)
     local cache = {}
     for _, table in pairs(scope_state.cache) do
-       cache[#cache+1] = utils.get_buffer_ids(table)
+        cache[#cache + 1] = utils.get_buffer_ids(table)
     end
-    require "scope.core".cache = cache
-    require "scope.core".last_tab = scope_state.last_tab
-    require "scope.core".on_tab_enter()
+    require("scope.core").cache = cache
+    require("scope.core").last_tab = scope_state.last_tab
+    require("scope.core").on_tab_enter()
 end
 
-function M.save_session()
+function M.serialize_state()
     local core = require "scope.core"
     local scope_cache = {}
     for k, v in pairs(core.cache) do
@@ -29,7 +26,17 @@ function M.save_session()
         cache = scope_cache,
         last_tab = core.last_tab,
     }
-    vim.g.ScopeState = vim.json.encode(state)
+    return vim.json.encode(state)
+end
+
+function M.load_session()
+    if vim.g.ScopeState ~= nil then
+        M.deserialize_state(vim.g.ScopeState)
+    end
+end
+
+function M.save_session()
+    vim.g.ScopeState = M.serialize_state()
 end
 
 return M
