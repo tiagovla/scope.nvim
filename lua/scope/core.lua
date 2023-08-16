30f9d01b4b2b72df9b0b9ee49bfef6fa11b8880b
Tuan Bui <tuanba1@gmail.com>
2023-08-17 01:04:17 +0700

    feat: add feature to move buffer between tabs (#15)
    * feat: add feature to move buffer between tabs
    
    have more friendly message when input wrong agument
    
    refactor(scope): separate function
    
    * fix: mismatch between tabnr and tab handle
    
    bufferline use tabnr to display, when scope.nvim use tabhandle to store
    data, so it's needed to convert
    
    * Add document for :ScopeMoveBuf command

--- lua/scope/core.lua
+++ lua/scope/core.lua
@@ -43,4 +43,49 @@ function M.print_summary()
     end
 end
 
+function M.move_current_buf(opts)
+    -- ensure current buflisted
+    local buflisted = vim.api.nvim_buf_get_option(0, "buflisted")
+    if not buflisted then
+        return
+    end
+
+    local target = tonumber(opts.args)
+    if target == nil then
+        -- invalid target tab, get input from user
+        local input = vim.fn.input("Move buf to: ")
+        if input == "" then -- user cancel
+            return
+        end
+
+        target = tonumber(input)
+    end
+
+    -- bufferline always display  tab number, not the handle. When scope use tab handle to store buffer info. So need to convert
+    local target_handle = vim.api.nvim_list_tabpages()[target]
+
+    if target_handle == nil then
+        vim.api.nvim_err_writeln("Invalid target tab")
+        return
+    end
+
+    M.move_buf(vim.api.nvim_get_current_buf(), target_handle)
+end
+
+function M.move_buf(bufnr, target)
+    -- copy current buf to target tab
+    local target_bufs = M.cache[target] or {}
+    target_bufs[#target_bufs + 1] = bufnr
+
+    -- remove current buf from current tab if it is not the last one in the tab
+    local buf_nums = utils.get_valid_buffers()
+    if #buf_nums > 1 then
+        vim.api.nvim_buf_set_option(bufnr, "buflisted", false)
+
+        -- current buf are not in the current tab anymore, so we switch to the previous tab
+        if bufnr == vim.api.nvim_get_current_buf() then
+            vim.cmd("bprevious")
+        end
+    end
+end
 return M
